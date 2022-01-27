8ea757f25cfce21594c771c568f9f3b5b146a68c
James Cranwell-Ward <jcranwellward@unicef.org>
2022-01-27 21:15:26 +0000

    tweaks

--- example.py
+++ example.py
@@ -12,11 +12,9 @@ from gilfoyle import report
 # ====================================================================================================================
 
 # Create a report
-pdf = report.Report(output='example.pdf')
+pdf = report.Report(output="example.pdf")
 
-pdf.set_title('Gilfoyle example')
-pdf.set_accent_background_color('#9f85ca')
-pdf.set_accent_font_color('#fff')
+pdf.set_title("Gilfoyle example")
 
 # Create an empty payload
 payload = pdf.get_payload()
@@ -25,191 +23,210 @@ payload = pdf.get_payload()
 # Chapter cover
 # ====================================================================================================================
 
-payload = pdf.add_page(payload,
-                       page_type='chapter',
-                       page_title='Gilfoyle',
-                       page_subheading='Various examples')
+payload = pdf.add_page(
+    payload,
+    page_type="chapter",
+    page_title="Gilfoyle",
+    page_subheading="Various examples",
+    page_message={"message": "This is a page message", "style": "warning"},
+)
 
 # ====================================================================================================================
 # Simple layout with dataframe
 # ====================================================================================================================
 
 # Load a Pandas dataframe and return 13 rows
-df = pd.read_csv('https://raw.githubusercontent.com/flyandlure/datasets/master/monthly-ecommerce-data.csv',
-                 skiprows=1,
-                 names=['Period', 'Sessions', 'Transactions', 'Conversion Rate', 'Revenue', 'AOV']).head(13)
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/flyandlure/datasets/master/monthly-ecommerce-data.csv",
+    skiprows=1,
+    names=["Period", "Sessions", "Transactions", "Conversion Rate", "Revenue", "AOV"],
+).head(13)
 
 # Add the dataframe to the payload
-payload = pdf.add_page(payload,
-                       page_type='report',
-                       page_layout='simple',
-                       page_title='Simple layout, dataframe',
-                       page_dataframe=df
-                       )
+payload = pdf.add_page(
+    payload,
+    page_type="report",
+    page_layout="simple",
+    page_title="Simple layout, dataframe",
+    page_dataframe=df,
+)
 
 # ====================================================================================================================
 # Simple layout with dataframe and metrics comparing last month to last year
 # ====================================================================================================================
 
 # Load a Pandas dataframe and return 13 rows
-df = pd.read_csv('https://raw.githubusercontent.com/flyandlure/datasets/master/monthly-ecommerce-data.csv',
-                 skiprows=1,
-                 names=['Period', 'Sessions', 'Transactions', 'Conversion rate', 'Revenue', 'AOV']).head(13)
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/flyandlure/datasets/master/monthly-ecommerce-data.csv",
+    skiprows=1,
+    names=["Period", "Sessions", "Transactions", "Conversion rate", "Revenue", "AOV"],
+).head(13)
 
 # Add metrics
 metrics = [
     pdf.add_metric_tile(
-        metric_title='Sessions',
-        metric_value_now=df['Sessions'].loc[0],
-        metric_value_before=df['Sessions'].loc[12],
-        metric_name='year'
+        metric_title="Sessions",
+        metric_value_now=df["Sessions"].loc[0],
+        metric_value_before=df["Sessions"].loc[12],
+        metric_name="year",
     ),
     pdf.add_metric_tile(
-        metric_title='Conversion rate',
-        metric_value_now=df['Conversion rate'].loc[0],
-        metric_value_before=df['Conversion rate'].loc[12],
-        metric_name='year',
-        metric_suffix='%'
+        metric_title="Conversion rate",
+        metric_value_now=df["Conversion rate"].loc[0],
+        metric_value_before=df["Conversion rate"].loc[12],
+        metric_name="year",
+        metric_suffix="%",
     ),
     pdf.add_metric_tile(
-        metric_title='Transactions',
-        metric_value_now=df['Transactions'].loc[0],
-        metric_value_before=df['Transactions'].loc[12],
-        metric_name='year'
+        metric_title="Transactions",
+        metric_value_now=df["Transactions"].loc[0],
+        metric_value_before=df["Transactions"].loc[12],
+        metric_name="year",
+        metric_color="danger",
     ),
     pdf.add_metric_tile(
-        metric_title='AOV',
-        metric_value_now=round(df['AOV'].loc[0], 2),
-        metric_value_before=df['AOV'].loc[12],
-        metric_name='year',
-        metric_prefix='£'
+        metric_title="AOV",
+        metric_value_now=round(df["AOV"].loc[0], 2),
+        metric_value_before=df["AOV"].loc[12],
+        metric_name="year",
+        metric_prefix="£",
+        metric_color="success",
     ),
     pdf.add_metric_tile(
-        metric_title='Revenue',
-        metric_value_now=df['Revenue'].loc[0],
-        metric_value_before=df['Revenue'].loc[12],
-        metric_name='year',
-        metric_prefix='£'
+        metric_title="Revenue",
+        metric_value_now=df["Revenue"].loc[0],
+        metric_value_before=df["Revenue"].loc[12],
+        metric_name="year",
+        metric_prefix="£",
+        metric_color="warning",
     ),
 ]
 
 # Add the dataframe to the payload
-payload = pdf.add_page(payload,
-                       page_type='report',
-                       page_layout='simple',
-                       page_title='Simple layout, dataframe, YoY metrics',
-                       page_dataframe=df,
-                       page_metrics=metrics,
-                       )
+payload = pdf.add_page(
+    payload,
+    page_type="report",
+    page_layout="simple",
+    page_title="Simple layout, dataframe, YoY metrics",
+    page_dataframe=df,
+    page_metrics=metrics,
+)
 
 # ====================================================================================================================
 # Simple layout with dataframe and metrics comparing last month to previous month
 # ====================================================================================================================
 
 # Load a Pandas dataframe and return 13 rows
-df = pd.read_csv('https://raw.githubusercontent.com/flyandlure/datasets/master/monthly-ecommerce-data.csv',
-                 skiprows=1,
-                 names=['Period', 'Sessions', 'Transactions', 'Conversion rate', 'Revenue', 'AOV']).head(13)
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/flyandlure/datasets/master/monthly-ecommerce-data.csv",
+    skiprows=1,
+    names=["Period", "Sessions", "Transactions", "Conversion rate", "Revenue", "AOV"],
+).head(13)
 
 # Add metrics
 metrics = [
     pdf.add_metric_tile(
-        metric_title='Sessions',
-        metric_value_now=df['Sessions'].loc[0],
-        metric_value_before=df['Sessions'].loc[1],
-        metric_name='month'
+        metric_title="Sessions",
+        metric_value_now=df["Sessions"].loc[0],
+        metric_value_before=df["Sessions"].loc[1],
+        metric_name="month",
     ),
     pdf.add_metric_tile(
-        metric_title='Conversion rate',
-        metric_value_now=df['Conversion rate'].loc[0],
-        metric_value_before=df['Conversion rate'].loc[1],
-        metric_name='month',
-        metric_suffix='%'
+        metric_title="Conversion rate",
+        metric_value_now=df["Conversion rate"].loc[0],
+        metric_value_before=df["Conversion rate"].loc[1],
+        metric_name="month",
+        metric_suffix="%",
     ),
     pdf.add_metric_tile(
-        metric_title='Transactions',
-        metric_value_now=df['Transactions'].loc[0],
-        metric_value_before=df['Transactions'].loc[1],
-        metric_name='month'
+        metric_title="Transactions",
+        metric_value_now=df["Transactions"].loc[0],
+        metric_value_before=df["Transactions"].loc[1],
+        metric_name="month",
     ),
     pdf.add_metric_tile(
-        metric_title='AOV',
-        metric_value_now=round(df['AOV'].loc[0], 2),
-        metric_value_before=df['AOV'].loc[1],
-        metric_name='month',
-        metric_prefix='£'
+        metric_title="AOV",
+        metric_value_now=round(df["AOV"].loc[0], 2),
+        metric_value_before=df["AOV"].loc[1],
+        metric_name="month",
+        metric_prefix="£",
     ),
     pdf.add_metric_tile(
-        metric_title='Revenue',
-        metric_value_now=df['Revenue'].loc[0],
-        metric_value_before=df['Revenue'].loc[1],
-        metric_name='month',
-        metric_prefix='£'
+        metric_title="Revenue",
+        metric_value_now=df["Revenue"].loc[0],
+        metric_value_before=df["Revenue"].loc[1],
+        metric_name="month",
+        metric_prefix="£",
     ),
 ]
 
 # Add the dataframe to the payload
-payload = pdf.add_page(payload,
-                       page_type='report',
-                       page_layout='simple',
-                       page_title='Simple layout, dataframe, MoM metrics',
-                       page_dataframe=df,
-                       page_metrics=metrics,
-                       )
+payload = pdf.add_page(
+    payload,
+    page_type="report",
+    page_layout="simple",
+    page_title="Simple layout, dataframe, MoM metrics",
+    page_dataframe=df,
+    page_metrics=metrics,
+)
 
 # ====================================================================================================================
 # Simple layout with dataframe and no metrics comparison
 # ====================================================================================================================
 
 # Load a Pandas dataframe and return 13 rows
-df = pd.read_csv('https://raw.githubusercontent.com/flyandlure/datasets/master/monthly-ecommerce-data.csv',
-                 skiprows=1,
-                 names=['Period', 'Sessions', 'Transactions', 'Conversion rate', 'Revenue', 'AOV']).head(13)
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/flyandlure/datasets/master/monthly-ecommerce-data.csv",
+    skiprows=1,
+    names=["Period", "Sessions", "Transactions", "Conversion rate", "Revenue", "AOV"],
+).head(13)
 
 # Add metrics
 metrics = [
     pdf.add_metric_tile(
-        metric_title='Sessions',
-        metric_value_now=df['Sessions'].loc[0],
+        metric_title="Sessions",
+        metric_value_now=df["Sessions"].loc[0],
     ),
     pdf.add_metric_tile(
-        metric_title='Conversion rate',
-        metric_value_now=df['Conversion rate'].loc[0],
-        metric_suffix='%'
+        metric_title="Conversion rate",
+        metric_value_now=df["Conversion rate"].loc[0],
+        metric_suffix="%",
     ),
     pdf.add_metric_tile(
-        metric_title='Transactions',
-        metric_value_now=df['Transactions'].loc[0],
+        metric_title="Transactions",
+        metric_value_now=df["Transactions"].loc[0],
     ),
     pdf.add_metric_tile(
-        metric_title='AOV',
-        metric_value_now=round(df['AOV'].loc[0], 2),
-        metric_prefix='£'
+        metric_title="AOV",
+        metric_value_now=round(df["AOV"].loc[0], 2),
+        metric_prefix="£",
     ),
     pdf.add_metric_tile(
-        metric_title='Revenue',
-        metric_value_now=df['Revenue'].loc[0],
-        metric_prefix='£'
+        metric_title="Revenue", metric_value_now=df["Revenue"].loc[0], metric_prefix="£"
     ),
 ]
 
 # Add the dataframe to the payload
-payload = pdf.add_page(payload,
-                       page_type='report',
-                       page_layout='simple',
-                       page_title='Simple layout, dataframe, metrics (no comparison)',
-                       page_dataframe=df,
-                       page_metrics=metrics,
-                       )
+payload = pdf.add_page(
+    payload,
+    page_type="report",
+    page_layout="simple",
+    page_title="Simple layout, dataframe, metrics (no comparison)",
+    page_dataframe=df,
+    page_metrics=metrics,
+    page_message={"message": "This is a page message", "style": "warning"},
+)
 
 # ====================================================================================================================
 # Left commentary layout with dataframe and notification message
 # ====================================================================================================================
 
 # Load a Pandas dataframe and return 13 rows
-df = pd.read_csv('https://raw.githubusercontent.com/flyandlure/datasets/master/monthly-ecommerce-data.csv',
-                 skiprows=1,
-                 names=['Period', 'Sessions', 'Transactions', 'Conversion Rate', 'Revenue', 'AOV']).head(13)
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/flyandlure/datasets/master/monthly-ecommerce-data.csv",
+    skiprows=1,
+    names=["Period", "Sessions", "Transactions", "Conversion Rate", "Revenue", "AOV"],
+).head(13)
 
 # Define commentary
 page_commentary = """
@@ -220,48 +237,54 @@ notification message to alert readers to specific points.
 """
 
 # Define message
-page_message = {'message': 'This is a page message', 'style': 'warning'}
+page_message = {"message": "This is a page message", "style": "warning"}
 
 # Add the dataframe to the payload
-payload = pdf.add_page(payload,
-                       page_type='report',
-                       page_layout='left-commentary',
-                       page_title='Left-commentary layout, dataframe, commentary, notification, warning message',
-                       page_dataframe=df,
-                       page_commentary=page_commentary,
-                       page_notification='This is a notification',
-                       page_message=page_message,
-                       )
+payload = pdf.add_page(
+    payload,
+    page_type="report",
+    page_layout="left-commentary",
+    page_title="Left-commentary layout, dataframe, commentary, notification, warning message",
+    page_dataframe=df,
+    page_commentary=page_commentary,
+    page_message=page_message,
+    page_metrics=metrics,
+)
 
 # ====================================================================================================================
 # Simple layout with dataframe
 # ====================================================================================================================
 
 # Load a Pandas dataframe and return 13 rows
-df = pd.read_csv('https://raw.githubusercontent.com/flyandlure/datasets/master/monthly-ecommerce-data.csv',
-                 skiprows=1,
-                 names=['Period', 'Sessions', 'Transactions', 'Conversion Rate', 'Revenue', 'AOV']).head(13)
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/flyandlure/datasets/master/monthly-ecommerce-data.csv",
+    skiprows=1,
+    names=["Period", "Sessions", "Transactions", "Conversion Rate", "Revenue", "AOV"],
+).head(13)
 
 # Define message
-page_message = {'message': 'This is a page message', 'style': 'danger'}
+page_message = {"message": "This is a page message", "style": "danger"}
 
 # Add the dataframe to the payload
-payload = pdf.add_page(payload,
-                       page_type='report',
-                       page_layout='simple',
-                       page_title='Simple layout, dataframe, danger message',
-                       page_dataframe=df,
-                       page_message=page_message,
-                       )
+payload = pdf.add_page(
+    payload,
+    page_type="report",
+    page_layout="simple",
+    page_title="Simple layout, dataframe, danger message",
+    page_dataframe=df,
+    page_message=page_message,
+)
 
 # ====================================================================================================================
 # Simple layout with dataframe and visualisation
 # ====================================================================================================================
 
 # Load a Pandas dataframe and return 13 rows
-df = pd.read_csv('https://raw.githubusercontent.com/flyandlure/datasets/master/monthly-ecommerce-data.csv',
-                 skiprows=1,
-                 names=['Period', 'Sessions', 'Transactions', 'Conversion Rate', 'Revenue', 'AOV']).head(13)
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/flyandlure/datasets/master/monthly-ecommerce-data.csv",
+    skiprows=1,
+    names=["Period", "Sessions", "Transactions", "Conversion Rate", "Revenue", "AOV"],
+).head(13)
 
 # Define commentary
 page_commentary = """
@@ -270,24 +293,25 @@ pass in the filename to the "page_visualisation" argument. You will want to twea
 """
 
 # Generate a visualisation and save the image
-sns.set(rc={'figure.figsize': (15, 6)})
-line_plot = sns.lineplot(x='Period', y='Sessions', data=df)
+sns.set(rc={"figure.figsize": (15, 6)})
+line_plot = sns.lineplot(x="Period", y="Sessions", data=df)
 line_plot.figure.savefig("lineplot.png")
 
 # Add the dataframe to the payload
-payload = pdf.add_page(payload,
-                       page_type='report',
-                       page_layout='plot',
-                       page_title='Plot layout, visualisation, commentary',
-                       page_dataframe=df,
-                       page_commentary=page_commentary,
-                       page_visualisation='lineplot.png',
-                       )
+payload = pdf.add_page(
+    payload,
+    page_type="report",
+    page_layout="plot",
+    page_title="Plot layout, visualisation, commentary",
+    page_dataframe=df,
+    page_commentary=page_commentary,
+    page_visualisation="lineplot.png",
+)
 
 # ====================================================================================================================
 # Render the report
 # ====================================================================================================================
 
 # Create the report
-pdf.create_report(payload, verbose=False, output='pdf')
-print('Created example.pdf')
+pdf.create_report(payload, verbose=False, output="pdf")
+print("Created example.pdf")
